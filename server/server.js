4d34962a6b4c3e2e546a62e3ae81c76bf8a9d370
janayAl <jsallen298705@gmail.com>
2021-10-07 18:56:26 -0500

    require apollo server express packages

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
