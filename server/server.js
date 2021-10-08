d86389d7a1855404e32f460c60195325ac05d01d
janayAl <jsallen298705@gmail.com>
2021-10-07 19:02:48 -0500

    added middleware for apollo server

--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
