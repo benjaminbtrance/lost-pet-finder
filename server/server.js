aee15ed9137a2a5519d58a3b7a741dcf351683b6
janayAl <jsallen298705@gmail.com>
2021-10-07 19:09:19 -0500

    updated port to reflect graphql in server

--- server/server.js
+++ server/server.js
@@ -24,5 +24,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`API server running on port ${PORT}`));
+  console.log(`Use GraphQL at http:localhost:${PORT}${server.graphqlPath}`);
 });
