2d15dd83f9cc71358b063e4fb8c0e78921f68d9e
janayAl <jsallen298705@gmail.com>
2021-10-07 19:06:45 -0500

    apply middleware app and remove route app in server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -22,8 +22,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
