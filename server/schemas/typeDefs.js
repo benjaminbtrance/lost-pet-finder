acee1feb46c178df97f8c7082534b4c30f1b39e8
janayAl <jsallen298705@gmail.com>
2021-10-09 12:26:08 -0500

    replaced thought with report in typedefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,13 +6,13 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    thoughts: [Thought]!
+    reports: [Report]!
   }
 
-  type Thought {
+  type Report {
     _id: ID
-    thoughtText: String
-    thoughtAuthor: String
+    reportText: String
+    reportAuthor: String
     createdAt: String
     comments: [Comment]!
   }
@@ -32,18 +32,18 @@ const typeDefs = gql`
   type Query {
     users: [User]
     user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
+    reports(username: String): [Report]
+    report(reportId: ID!): Report
     me: User
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    addReport(reportText: String!): Report
+    addComment(reportId: ID!, commentText: String!): Report
+    removeThought(thoughtId: ID!): Report
+    removeComment(thoughtId: ID!, commentId: ID!): Report
   }
 `;
 
