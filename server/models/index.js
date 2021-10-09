b6b1f7b8e020603597479478629b1a00f0ba027a
janayAl <jsallen298705@gmail.com>
2021-10-09 12:29:33 -0500

    replaced thought with report and renamed filed to repor.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Report = require('./Report');
 
-module.exports = { User, Thought };
+module.exports = { User, Report };
