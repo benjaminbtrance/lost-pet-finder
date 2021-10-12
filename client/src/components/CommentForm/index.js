1e4289ae52541e82a55988e3d130a84040a8cf7c
janayAl <jsallen298705@gmail.com>
2021-10-12 16:23:04 -0500

    replaced thought with word again in the index of the commentform

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -41,7 +41,7 @@ const CommentForm = ({ reportId }) => {
 
   return (
     <div>
-      <h4>What are your thoughts on this thought?</h4>
+      <h4>What are your thoughts on this report?</h4>
 
       {Auth.loggedIn() ? (
         <>
@@ -77,7 +77,7 @@ const CommentForm = ({ reportId }) => {
         </>
       ) : (
         <p>
-          You need to be logged in to share your thoughts. Please{' '}
+          You need to be logged in to share your reports. Please{' '}
           <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
         </p>
       )}
