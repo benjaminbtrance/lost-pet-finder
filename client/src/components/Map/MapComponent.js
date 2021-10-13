9b7bce7fa99e1bd02674bce0b20763cec9bd0589
Kris Tina <kristyguerrero20@gmail.com>
2021-10-12 20:37:27 -0500

    got the geolocation to work!

--- client/src/components/Map/MapComponent.js
+++ client/src/components/Map/MapComponent.js
@@ -9,90 +9,95 @@ const mapStyle = {
   height: 600,
 };
 
-mapboxgl.accessToken =
+const mapboxApiKey =
   'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw';
 
-// const params = {
-//   country: 'us',
-// };
+const params = {
+  country: 'us',
+};
 
-// class MapView extends PureComponent {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       viewport: {
-//         latitude: 45.50884,
-//         longitude: -73.58781,
-//         zoom: 15,
-//       },
-//     };
-//   }
+class MapView extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 45.50884,
+        longitude: -73.58781,
+        zoom: 15,
+      },
+    };
+  }
 
-//   onSelected = (viewport, item) => {
-//     this.setState({
-//       viewport,
-//     });
-//   };
+  onSelected = (viewport, item) => {
+    this.setState({
+      viewport,
+    });
+  };
 
-//   render() {
-//     const { viewport } = this.state;
-//     return (
-//       <Container fluid={true}>
-//         <Row>
-//           <Col>
-//             <h2>Search</h2>
-//           </Col>
-//         </Row>
-//         <Row className="py-4">
-//           <Col xs={2}>
-//             <Geocoder
-//               mapboxApiAccessToken={
-//                 'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw'
-//               }
-//               onSelected={this.onSelected}
-//               viewport={viewport}
-//               hideOnSelect={true}
-//               value=""
-//               queryParams={params}
-//             />
-//           </Col>
-//         </Row>
-//         <Row>
-//           <Col>
-//             <ReactMapGL
-//               mapboxApiAccessToken={
-//                 'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw'
-//               }
-//               mapStyle="mapbox://styles/mapbox/streets-v11"
-//               {...viewport}
-//               {...mapStyle}
-//               onViewportChange={(viewport) => this.setState({ viewport })}
-//             ></ReactMapGL>
-//           </Col>
-//         </Row>
-//       </Container>
-//     );
-//   }
-// }
-
-// export default MapView;
-
-export default function MapComponent() {
-  let [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  return (
-    <ReactMapGL
-      mapboxApiAccessToken={
-        'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw'
-      }
-      {...viewport}
-      onViewportChange={(newView) => setViewport(newView)}
-    ></ReactMapGL>
-  );
+  render() {
+    const { viewport } = this.state;
+    return (
+      <Container fluid={true}>
+        <Row>
+          <Col>
+            <h2>Search</h2>
+          </Col>
+        </Row>
+        <Row className="py-4">
+          <Col xs={2}>
+            <Geocoder
+              mapboxApiAccessToken={
+                'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw'
+              }
+              onSelected={this.onSelected}
+              viewport={viewport}
+              hideOnSelect={true}
+              value=""
+              queryParams={params}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ReactMapGL
+              mapboxApiAccessToken={
+                'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw'
+              }
+              mapStyle="mapbox://styles/mapbox/streets-v11"
+              {...viewport}
+              {...mapStyle}
+              onViewportChange={(viewport) => this.setState({ viewport })}
+            ></ReactMapGL>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
+
+export default MapView;
+
+// Commented stuff is previous map but will leave in here just in case :)
+
+// mapboxgl.accessToken =
+//   'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw';
+
+// export default function MapComponent() {
+//   let [viewport, setViewport] = useState({
+//     latitude: 37.7577,
+//     longitude: -122.4376,
+//     zoom: 8,
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//   });
+
+//   return (
+//     <ReactMapGL
+//       mapboxApiAccessToken={
+//         'pk.eyJ1Ijoia3Jpc3R5Z3VlcnJlcm8yMCIsImEiOiJja3Vob2U1MnYyZmxvMndvOHIzMzNzMnZiIn0.WY_nDAowRmL88PNZs7iJRw'
+//       }
+//       {...viewport}
+//       onViewportChange={(newView) => setViewport(newView)}
+//     ></ReactMapGL>
+//   );
+// }
